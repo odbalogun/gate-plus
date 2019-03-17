a65dadaa6c1679555fc6166720dacd01be1833cd
Oduntan Balogun <oduntan@live.com>
2019-03-17 01:16:09 +0100

    Added slug field

--- estates/models.py
+++ estates/models.py
@@ -6,7 +6,7 @@ from .managers import EstateManager
 # Create your models here.
 class Estate(TenantMixin):
     name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100)
+    slug = models.CharField(max_length=20)
     is_active = models.BooleanField(default=True)
     created_on = models.DateField(auto_now_add=True)
 
