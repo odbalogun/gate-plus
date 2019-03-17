3b610401c040d92e8094421a26bf2c3fc5742a92
Oduntan Balogun <oduntan@live.com>
2019-03-17 01:02:00 +0100

    Modified models.py. Added is_status and created fields. Added custom manager

--- estates/models.py
+++ estates/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from tenant_schemas.models import TenantMixin
+from .managers import EstateManager
 
 
 # Create your models here.
 class Estate(TenantMixin):
     name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateField(auto_now_add=True)
 
     auto_create_schema = True
+
+    objects = models.Manager()
+    tenancy = EstateManager()
